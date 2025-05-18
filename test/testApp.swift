4f4385b428104002751e770bb28a62c84ff4800a
adbak <adbak.dev@gmail.com>
2025-05-18 21:54:07 +0200

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Adnan Bakaev  on 18/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
